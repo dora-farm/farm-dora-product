67d6bfe93df3f5cb42221c8f9b79f591067edefc
kjh <kjhwinter@gmail.com>
2025-04-24 17:33:54 +0900

    chore: yml 파일에 암호화(jasypt)된 정보 추가

--- src/main/java/com/farmdora/farmdoraproductmanagement/FarmDoraProductManagementApplication.java
+++ src/main/java/com/farmdora/farmdoraproductmanagement/FarmDoraProductManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.home}/config/farm-dora.properties")
 public class FarmDoraProductManagementApplication {
 
     public static void main(String[] args) {
